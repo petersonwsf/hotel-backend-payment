a2c6e3c3fc9ed883bd7b8aee0cd90a17329cafb2
Peterson William <petersondev.profissional@gmail.com>
2026-09-13 11:02:17 -0300

    feat: adicionando escuta para reembolso de reservas canceladas

--- src/modules/consumer/controller/ConsumerController.ts
+++ src/modules/consumer/controller/ConsumerController.ts
@@ -2,6 +2,14 @@ import { Controller, Logger } from '@nestjs/common';
 import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 import { ReservationEventTypes } from '../types/EventTypes';
 import { PaymentService } from 'src/modules/payment/service/PaymentService';
+import type {
+  MessageDataEnvelope,
+  ReservationDataMessage,
+} from '../types/ReservationMessage';
+import { PaymentNotFound } from 'src/modules/payment/domain/errors/PaymentNotFound';
+import { PaymentNotBelongUser } from 'src/modules/payment/domain/errors/PaymentNotBelongUser';
+import { PaymentCannotBeRefunded } from 'src/modules/payment/domain/errors/PaymentCannotBeRefunded';
+import { ReservationPaymentNotFound } from 'src/modules/payment/domain/errors/ReservationPaymentNotFound';
 
 @Controller()
 export class ConsumerController {
@@ -11,7 +19,7 @@ export class ConsumerController {
 
   @EventPattern(ReservationEventTypes.RESERVATION_CANCELLED)
   async handleCancelReservation(
-    @Payload() data: any,
+    @Payload() data: MessageDataEnvelope<ReservationDataMessage>,
     @Ctx() context: RmqContext,
   ) {
     const channel = context.getChannelRef();
@@ -22,17 +30,35 @@ export class ConsumerController {
         `Processando evento '${data.eventType}' | correlationId: ${data.correlationId}`,
       );
 
-      console.log(data);
+      const payment = await this.paymentService.findByReservationId(
+        data.data.id.toString(),
+      );
+
+      if (payment) {
+        await this.paymentService.refund({
+          id: payment.id.toString(),
+          user: data.data.user,
+        });
+      }
 
       channel.ack(originalMsg);
     } catch (error) {
-      this.logger.error(
-        `Erro ao processar evento '${data?.eventType}': ${error}`,
-      );
-
-      // false, false = não reenvia pra fila (evita loop infinito de erro)
-      // troque o segundo "false" pra "true" se quiser reenfileirar em caso de falha transitória
-      channel.nack(originalMsg, false, false);
+      if (
+        error instanceof PaymentNotFound ||
+        error instanceof PaymentNotBelongUser ||
+        error instanceof PaymentCannotBeRefunded ||
+        error instanceof ReservationPaymentNotFound
+      ) {
+        this.logger.warn(
+          `Evento '${data?.eventType}' descartado devido a erro de negócio irrecuperável: ${error.message} | correlationId: ${data?.correlationId}`,
+        );
+        channel.ack(originalMsg);
+      } else {
+        this.logger.error(
+          `Erro operacional ao processar evento '${data?.eventType}': ${error}`,
+        );
+        channel.nack(originalMsg, false, false);
+      }
     }
   }
 }
